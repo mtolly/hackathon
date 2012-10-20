dfed040535179fc82c917858ce6bdc18d32aa148
katamaritaco <katamaritaco@yahoo.com>
2012-10-20 06:20:16 -0500

    Added question/moved scoreboard

--- Hackathon/Hackathon/Hackathon/Game1.cs
+++ Hackathon/Hackathon/Hackathon/Game1.cs
@@ -283,10 +283,14 @@ namespace Hackathon
                 spriteBatch.Draw(AllPlates[i].plateContents, new Vector2((int)Math.Ceiling(AllPlates[i].x_value + (150 * scale_factor - (1.25 * AllPlates[i].plateContents.Width * (scale_factor)))), (int)Math.Ceiling(AllPlates[i].y_value - 100)), null, AllPlates[i].plateColor, 0f, Vector2.Zero, new Vector2((((float)AllPlates[i].y_value + 170) / (670)), (((float)AllPlates[i].y_value + 170) / (670))), SpriteEffects.None, 0f); 
             }
 
-            spriteBatch.Draw(cursor, new Vector2(mouse_x, mouse_y), mouse_down ? Color.Red : Color.White);
-            spriteBatch.Draw(scoreboard, new Vector2(45, 51), Color.White);
-            spriteBatch.DrawString(font, "Score: " + currentScore, new Vector2(50, 50), Color.MintCream);
+            
+
+            spriteBatch.Draw(question, new Vector2(270, 125), Color.White);
+
+            spriteBatch.Draw(scoreboard, new Vector2(310, 15), Color.White);
+            spriteBatch.DrawString(font, "Score: " + currentScore, new Vector2(313, 15), Color.MintCream);
             //spriteBatch.DrawString(font, inZone ? "true" : "false", new Vector2(400, 400), Color.Teal);
+            spriteBatch.Draw(cursor, new Vector2(mouse_x, mouse_y), mouse_down ? Color.Red : Color.White);
             spriteBatch.End();
 
             base.Draw(gameTime);
