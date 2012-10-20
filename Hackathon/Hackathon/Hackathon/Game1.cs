d335630cdb2d5c5efe7079b47678144c99a1b9c2
Mitchell Lutzke <mitchell649@hotmail.com>
2012-10-20 00:47:05 -0500

    Plate moved into its own separate file

--- Hackathon/Hackathon/Hackathon/Game1.cs
+++ Hackathon/Hackathon/Hackathon/Game1.cs
@@ -9,24 +9,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-class Plate
-{
-    public Plate(int xOrgin, int yOrgin, double xSpeed, double ySpeed)
-    {
-        x_value = xOrgin;
-        y_value = yOrgin;
-        x_speed = xSpeed;
-        y_speed = ySpeed;
-        x_orgin = xOrgin;
-        y_orgin = yOrgin;
-    }
-    public int x_value { get; set; }
-    public int y_value { get; set; }
-    public double x_speed { get; set; }
-    public double y_speed { get; set; }
-    public int x_orgin { get; set; }
-    public int y_orgin { get; set; }
-}
 
 namespace Hackathon
 {
---
+++ Hackathon/Hackathon/Hackathon/Plate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hackathon
+{
+    public class Plate : Microsoft.Xna.Framework.Game
+    {
+        public Plate(int xOrgin, int yOrgin, double xSpeed, double ySpeed)
+        {
+            x_value = xOrgin;
+            y_value = yOrgin;
+            x_speed = xSpeed;
+            y_speed = ySpeed;
+            x_orgin = xOrgin;
+            y_orgin = yOrgin;
+        }
+        public int x_value { get; set; }
+        public int y_value { get; set; }
+        public double x_speed { get; set; }
+        public double y_speed { get; set; }
+        public int x_orgin { get; set; }
+        public int y_orgin { get; set; }
+    }
+}
