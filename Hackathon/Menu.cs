a9763d44e2cfee7840a373f869213652a5330eda
katamaritaco <katamaritaco@yahoo.com>
2012-10-20 09:34:05 -0500

    What last update was supposed to be...

--- Hackathon/Menu.cs
+++ Hackathon/Menu.cs
@@ -12,7 +12,7 @@ public class Menu
 	public Menu()
 
 	{
-        Title = "Pong Clone";
+        Title = "Sushi Sensei's Game";
         MenuItems = new List<string>();
         MenuItems.Add("Single Player");
         MenuItems.Add("Multi Player");
@@ -33,6 +33,39 @@ public class Menu
             if (iterator > MenuItems.Count - 1) iterator = MenuItems.Count - 1;
             if (iterator < 0) iterator = 0;
         }
+    }
+
+    public int GetNumberOfOptions()
+    {
+        return MenuItems.Count;
+    }
+
+    public string GetItem(int index)
+    {
+        return MenuItems[index];
+    }
+
+    public void DrawMenu(SpriteBatch batch, int screenWidth, SpriteFont arial)
+    {
+        batch.DrawString(arial, Title, new Vector2(screenWidth / 2 - arial.MeasureString(Title).X / 2, 20), Color.White);
+        int yPos = 100;
+        for (int i = 0; i < GetNumberOfOptions(); i++)
+        {
+            Color colour = Color.White;
+            if (i == Iterator)
+            {
+                colour = Color.Gray;
+            }
+            batch.DrawString(arial, GetItem(i), new Vector2(screenWidth / 2 - arial.MeasureString(GetItem(i)).X / 2, yPos), colour);
+            yPos += 50;
+        }
+    }
+
+    public void DrawEndScreen(SpriteBatch batch, int screenWidth, SpriteFont arial)
+    {
+        batch.DrawString(arial, InfoText, new Vector2(screenWidth / 2 - arial.MeasureString(InfoText).X / 2, 300), Color.White);
+        string prompt = "Press Enter to Continue";
+        batch.DrawString(arial, prompt, new Vector2(screenWidth / 2 - arial.MeasureString(prompt).X / 2, 400), Color.White);
     }
  
 }
